8a076fb64c40cd4ba60a33308a5cf6715667328f
SubhamSengupta <sengupta335@gmail.com>
2017-07-09 12:16:55 +0530

    add new event emitter and listeners

--- server/server.js
+++ server/server.js
@@ -11,9 +11,21 @@ var io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'subham@example.com',
+    text: 'Hi there!',
+    createdAt: 123
+  });
+
+  socket.on('createMessage',(message) => {
+    console.log('new meassge', message);
+  });
+
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
+
+
 });
 
 app.use(express.static(publicPath));
